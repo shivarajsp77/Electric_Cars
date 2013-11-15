ccf10b5df81ad45c0ddf4fecf8246735513649e7
serah <serah4291@gmail.com>
2013-11-15 15:02:31 -0800

    changed gitignore

--- electricCars/electricCars/settings.py
+++ electricCars/electricCars/settings.py
@@ -60,8 +60,11 @@ WSGI_APPLICATION = 'electricCars.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ecars',
+        'USER': 'serah',
+        'PASSWORD': '272',
+        'HOST': 'localhost',
     }
 }
 
